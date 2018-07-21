9cdc025f6e7c24b031b2aecbe68ef58f1f45861d
adarshshetty997 <37131193+adarshshetty997@users.noreply.github.com>
2018-07-22 05:10:44 +0530

    Chatroom1 New

--- ChatListActivity.java
+++ ChatListActivity.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -50,8 +51,10 @@ public class ChatListActivity extends AppCompatActivity {
                 Object o = dataSnapshot.child(mUser1).getValue();
                 for(DataSnapshot ds: dataSnapshot.getChildren()){
                     String val = ds.getValue().toString();
+
                     String v2 = ds.getValue(UsersInfo.class).getUser_name();
-                    if(!val.contains("ad_title")&&!ds.getValue().equals(o))
+                    if(v2!=null && !val.contains("ad_title")&&!ds.getValue().equals(o))
+
                         arrayList.add(v2);
                     arrayAdapter.notifyDataSetChanged();
                 }
